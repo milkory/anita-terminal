e13029ebe0141d9c9abbb418c338592b22ab4bd9
Milk Ory <milkory@outlook.com>
2024-03-27 21:55:52 +0800

    fix: fix server data storage

--- server/api/char.ts
+++ server/api/char.ts
@@ -1,15 +1,12 @@
-import fs from 'node:fs';
-import path from 'node:path';
+const storage = useStorage('assets:server');
 
-const base = process.env.NODE_ENV == 'production' ? '/data/char' : '/public/data/char';
-
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   setHeader(event, 'Content-Type', 'application/json');
   const query = getQuery(event);
   if (query.id) {
-    const file = path.join(process.cwd(), `${base}/${query.id}.json`);
-    if (fs.existsSync(file)) {
-      return fs.readFileSync(file, 'utf-8');
+    const path = `data/char/${query.id}.json`;
+    if (await storage.hasItem(path)) {
+      return await useStorage('assets:server').getItem(path);
     }
   }
   return {};
