f0fdbe6d57f6dce9dd4e09cb65c3d50fe8cb8e49
Milk Ory <milkory@outlook.com>
2024-03-27 21:21:02 +0800

    fix: fix vercel deploy

--- server/api/char.ts
+++ server/api/char.ts
@@ -1,11 +1,13 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
+const base = process.env.NODE_ENV == 'production' ? '/data/char' : '/public/data/char';
+
 export default defineEventHandler((event) => {
   setHeader(event, 'Content-Type', 'application/json');
   const query = getQuery(event);
   if (query.id) {
-    const file = path.join(process.cwd(), `/public/data/char/${query.id}.json`);
+    const file = path.join(process.cwd(), `${base}/${query.id}.json`);
     if (fs.existsSync(file)) {
       return fs.readFileSync(file, 'utf-8');
     }
