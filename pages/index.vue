c3ea1f59a0f7138b20f475bf015ab4a5f263ec27
Milk Ory <milkory@outlook.com>
2024-03-31 23:41:36 +0800

    feat: add welcome page

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,17 @@
-<template><div></div></template>
+<template>
+  <div class="rs-welcome h-full fixed-top w-100 d-flex align-items-center justify-content-around">
+    <div>
+      <img src="/img/welcome.webp" alt="欢迎" />
+      <div>123</div>
+    </div>
+  </div>
+</template>
+
+<style>
+.rs-welcome {
+  img {
+    min-width: 25%;
+    max-width: 350px;
+  }
+}
+</style>
