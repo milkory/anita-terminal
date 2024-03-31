eeea54d495dead75817a1a91ddaff79461f5dc5b
Milk Ory <milkory@outlook.com>
2024-03-31 11:39:24 +0800

    fix: material link img not showing correct

--- components/link/Material.vue
+++ components/link/Material.vue
@@ -1,28 +1,25 @@
 <script setup lang="ts">
 const props = defineProps<{
   data: SourceMaterial;
-  width?: string;
+  width?: number;
 }>();
 
-const width = props.width || '36px';
+const width = props.width || 36;
+const image = useImage();
+const matImg = image(`/img/mat/${props.data.cId}.png`, { width: width });
 </script>
 
 <template>
   <div
     v-b-popover.hover="data.des"
-    class="link-material d-inline align-center py-1"
-    :style="{ lineHeight: width }"
+    class="link-material d-inline py-1"
+    :style="{ lineHeight: `${width}px` }"
   >
-    <div :class="['link-material-img', `bg-quality-${data.quality}`]" :style="{ width: width }">
-      <NuxtPicture
-        fit="contain"
-        :src="`/img/mat/${data.cId}.png`"
-        width="36px"
-        height="36px"
-        :modifiers="{ b: qualityColorMap[data.quality] }"
-      />
-    </div>
-    <span class="link-material-name ms-2 align-center">
+    <div
+      :class="['link-material-img', `bg-quality-${data.quality}`, ' align-middle']"
+      :style="{ width: `${width}px`, backgroundImage: `url(${matImg})` }"
+    ></div>
+    <span class="link-material-name ms-2 align-middle">
       {{ data.name }}
     </span>
   </div>
@@ -38,5 +35,8 @@ const width = props.width || '36px';
   border-radius: 50%;
   overflow: clip;
   aspect-ratio: 1;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
 }
 </style>
--- utils/common.ts
+++
@@ -1,7 +0,0 @@
-export const qualityColorMap = {
-  n: '#ffffff',
-  r: '#377bf8',
-  sr: '#c895ff',
-  ssr: '#ffb800',
-  ur: '#ff8a00'
-};
