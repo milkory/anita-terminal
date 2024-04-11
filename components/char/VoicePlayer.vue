10edfc28a2c319158cdafbc97222a28aa9281104
Milk Ory <milkory@outlook.com>
2024-04-11 13:36:41 +0800

    fix: multi voice playing bug

--- components/char/VoicePlayer.vue
+++ components/char/VoicePlayer.vue
@@ -12,12 +12,19 @@ const playing = computed(() => voicePlaying.value == identifier);
 
 let audio: HTMLAudioElement;
 
+watch(playing, (value) => {
+  if (!value && audio) {
+    console.log(value);
+    audio.pause();
+  }
+});
+
 function play() {
   voicePlaying.value = `${props.lang}-${props.voiceId}`;
 
   if (!audio) {
     audio = new Audio(`https://static.anita.wiki/audio/res/${props.voiceId}/${props.lang}.wav`);
-    audio.addEventListener('pause', () => (voicePlaying.value = undefined));
+    audio.addEventListener('pause', () => stop());
   } else {
     audio.currentTime = 0;
   }
@@ -25,15 +32,22 @@ function play() {
 }
 
 function stop() {
-  voicePlaying.value = undefined;
+  if (voicePlaying.value == identifier) {
+    voicePlaying.value = undefined;
+  }
   audio.pause();
 }
 </script>
 
 <template>
   <div class="char-voice-play d-inline-block ms-1">
-    <Icon v-if="!playing" :name="lang.icon" class="fs-5 btn-play" @click="play()" />
-    <Icon v-else name="material-symbols:pause-circle" class="fs-5 btn-pause" @click="stop()" />
+    <Icon v-if="!playing" :name="lang.icon" class="fs-5 btn-play align-sub" @click="play()" />
+    <Icon
+      v-else
+      name="material-symbols:pause-circle"
+      class="fs-5 btn-pause align-sub"
+      @click="stop()"
+    />
   </div>
 </template>
 
