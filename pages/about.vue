75bda1775a218d7fb2dde8ff06ed4d4e92655d13
Milk Ory <milkory@outlook.com>
2024-05-09 16:15:13 +0800

    feat: 5/9 final data update

--- pages/about.vue
+++ pages/about.vue
@@ -20,18 +20,25 @@ onMounted(() => setLastView('about'));
         <div class="col-md-8 overflow-y-auto flex-1">
           <div class="h-100">
             <div class="fs-5 fw-bold mb-3">更新日志</div>
-            <div class="fw-bold font-title text-warning">2024/4/11 - V1.2</div>
+            <div class="fw-bold font-title text-warning">2024/5/9</div>
             <ul>
-              <li>角色页面中新添加语音播放功能。</li>
+              <li class="text-danger">
+                最后的数据更新已完成。由于阿妮塔内部项目结构调整，本终端维护正式暂停。
+                <div class="fs-small text-secondary">（不涉及任何现实组织和人员）</div>
+              </li>
+            </ul>
+            <div class="fw-bold font-title text-secondary">2024/4/11 - V1.2</div>
+            <ul>
+              <li>角色页面中新增语音播放功能。</li>
               <li>重制错误页面（404）。</li>
             </ul>
             <div class="fw-bold font-title text-secondary">2024/4/8 - V1.1</div>
             <ul>
-              <li>物品页面中新添加词缀系列功能。</li>
+              <li>物品页面中新增词缀系列功能。</li>
             </ul>
             <div class="fw-bold font-title text-secondary">2024/4/6 - V1.0</div>
             <ul>
-              <li>重制关于页，添加更新日志。</li>
+              <li>重制关于页，新增更新日志。</li>
             </ul>
           </div>
         </div>
